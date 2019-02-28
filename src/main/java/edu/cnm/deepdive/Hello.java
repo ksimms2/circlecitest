c06bf93caa153eba3d85d9f6b4230a614184c9d7
Kevin Simms <ksimms2@cnm.edu>
2019-02-28 13:44:03 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/Hello.java
@@ -0,0 +1,11 @@
+package edu.cnm.deepdive;
+
+public class Hello {
+
+  public String sayHello(){
+    return "Hello";
+
+  }
+
+
+}
---
+++ src/test/java/edu/cnm/deepdive/HelloTest.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+  @Test
+  public void sayHello(){
+    assertEquals("Hello", new Hello().sayHello());
+  }
+
+}
